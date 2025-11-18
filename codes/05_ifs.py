876bdb627888b9b6e7f837ef021f9d8e1a136538
jinh2kakao <jinh2kakao@gmail.com>
2025-11-18 12:46:55 +0900

    first commit

---
+++ codes/05_ifs.py
@@ -0,0 +1,22 @@
+first = 3
+# if first >= 5:
+#     print("first is five")
+#     print("End of program.")
+# else:
+#     print("hello world!")
+#     print("End of world!")
+
+second = 6
+
+first = int(input("input first number: "))
+second = int(input("input second number: "))
+
+if first >= second:
+    print("hello world!")
+    print("End of world!")
+else:
+    print("first is less than second")
+    print("End of program.")
+
+# Output:
+# hello world!
---
+++ codes/06_whiles.py
@@ -0,0 +1,10 @@
+first = 5
+
+# while 문장구조
+# while condition:
+#    executable_statement(s)
+
+while first > 0 :
+    print("while 값 :", first)
+    first = first - 1
+print("while 종료")
