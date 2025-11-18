e1af6b4d5d6bc3899b87073cc492e532c2a01a0b
jinh2kakao <jinh2kakao@gmail.com>
2025-11-18 13:15:21 +0900

    update

--- codes/06_whiles.py
+++ codes/06_whiles.py
@@ -5,6 +5,10 @@ first = 5
 #    executable_statement(s)
 
 while first > 0 :
-    print("while 값 :", first)
+    print("while 값 :"+str(first))
+    if first == 3 :
+        print("while 중단")
+        break
     first = first - 1
+
 print("while 종료")
