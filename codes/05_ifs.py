7076502115330e40936f63e53a679455862f2fb0
jinh2kakao <jinh2kakao@gmail.com>
2025-11-18 17:40:18 +0900

    update

--- codes/05_ifs.py
+++ codes/05_ifs.py
@@ -1,4 +1,4 @@
-first = 3
+# first = 3
 # if first >= 5:
 #     print("first is five")
 #     print("End of program.")
@@ -6,17 +6,40 @@ first = 3
 #     print("hello world!")
 #     print("End of world!")
 
-second = 6
+# second = 6
 
-first = int(input("input first number: "))
-second = int(input("input second number: "))
+# first = int(input("input first number: "))
+# second = int(input("input second number: "))
 
-if first >= second:
-    print("hello world!")
-    print("End of world!")
-else:
-    print("first is less than second")
-    print("End of program.")
+# if first >= second:
+#     print("hello world!")
+#     print("End of world!")
+# else:
+#     print("first is less than second")
+#     print("End of program.")
 
 # Output:
 # hello world!
+
+# if condtion:
+#     excuted code
+
+# age = (input("Enter your age: "))
+# if age < 4 :
+#     print("You are ticket is free!")
+# elif age < 18 :
+#     print("You ticket is $5.")
+# else :
+#     print("You ticket is &10.")
+
+# print("end of program.")
+
+pass
+
+forth = "welcome to python!"
+forth_word = "python"
+if forth_word in forth:
+    print("yes, 'python' is here!")
+
+
+#"python" in "welcome to python!"
